97cbe33abe93fb47bc1635024e2107fb7dca34e4
Hanlunzheng <54917812+Hanlunzheng@users.noreply.github.com>
2024-10-17 19:25:17 +0000

    second frontend

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template,jsonify
+
 
 
 
 app = Flask(__name__)
 
-@app.route('/hello')
+
+JOBS =[
+
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'CollegePark, Maryland',
+    # 'salary':'$100,000',
+  },
+  {
+    'id':2,
+    'title':'Frontend Engineer',
+    'location':'CollegePark, Maryland',
+    'salary':'$120,000',
+  },
+{
+  'id':3,
+  'title':'Backend Engineer',
+  'location':'CollegePark, Maryland',
+  'salary':'$140,000',
+}
+]
+
+@app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('home.html',jobs = JOBS, company_name ="Aaron")
+  
+@app.route('/api/jobs')
+def list_job():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
